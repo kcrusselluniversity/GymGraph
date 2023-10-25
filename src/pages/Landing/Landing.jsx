a04b9530b249667a093ed9cba48f4ee2b8f60741
kcrusselluniversity <kcrussell.university@gmail.com>
2023-10-26 06:34:42 +1100

    Style landing page

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -2,11 +2,13 @@ import { Link } from "react-router-dom";
 
 const Landing = () => {
     return (
-        <>
-            <h1>Landing</h1>
-            <Link to="/signin">Sign in</Link>
-            <Link to="/signup">Sign up</Link>
-        </>
+        <div className="landing">
+            <main>
+                <h1>Landing</h1>
+                <Link to="/signin">Sign in</Link>
+                <Link to="/signup">Sign up</Link>
+            </main>
+        </div>
     );
 };
 
