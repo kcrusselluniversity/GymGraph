3f1466ab9032731ef2e9a7579ff021900b297d30
kcrusselluniversity <kcrussell.university@gmail.com>
2023-10-26 12:18:53 +1100

    Add logo and content to Landing page

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,13 +1,33 @@
 import { Link } from "react-router-dom";
+import logo from "../../assets/Logo.svg";
 
 const Landing = () => {
     return (
         <div className="landing">
-            <main>
-                <h1>Landing</h1>
-                <Link to="/signin">Sign in</Link>
-                <Link to="/signup">Sign up</Link>
-            </main>
+            <div className="landing__content">
+                <header>
+                    <img src={logo} alt="GymGraph" className="gymGraphLogo" />
+                </header>
+                <div className="content__main">
+                    <h1>Discover Strength Unleashed.</h1>
+                    <h4>
+                        Track, analyze, and optimize your weight lifting
+                        journey.
+                    </h4>
+                </div>
+                <div className="content__CTA">
+                    <h2>Your gains. Your data. One app.</h2>
+                </div>
+                <footer className="content__footer">
+                    <p>
+                        Already a member <Link to="/signin">Sign in</Link>
+                    </p>
+                    <p>
+                        ... or just looking for a demo
+                        <Link to="/signup">Sign up</Link>
+                    </p>
+                </footer>
+            </div>
         </div>
     );
 };
