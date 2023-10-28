a7a7c5480a7b4f27cbbe6362794c9c0ebf6c479b
kcrusselluniversity <kcrussell.university@gmail.com>
2023-10-28 14:50:05 +1100

    Code Sign up page component

--- src/pages/Signup/Signup.jsx
+++ src/pages/Signup/Signup.jsx
@@ -1,7 +1,24 @@
-const Signup = () => {
-    return <>
-    <h1>Signup</h1>
-    </>
-}
+import "./signup.css";
+import logo from "../../assets/Logo.svg";
+import Divider from "../../components/ui/Divider";
 
-export default Signup
+const Signup = () => {
+    return (
+        <div className="signUpPage">
+            <header>
+                <img src={logo} alt="GymGraph" className="gymGraphLogo" />
+            </header>
+            <div className="signUpPage__content">
+                <h1>Create your GymGraph account</h1>
+                <div className="content__signUpContainer">
+                    <form></form>
+                    <Divider text="or" direction="horizontal" />
+                    <div className="signInGoogle">Sign Up with Google</div>
+                </div>
+
+            </div>
+        </div>
+    );
+};
+
+export default Signup;
