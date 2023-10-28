f1dd56d68a7b4ca474e3f2e5b4bc94414cd27198
kcrusselluniversity <kcrussell.university@gmail.com>
2023-10-29 09:25:58 +1100

    Create "with google" button component

---
+++ src/components/ui/WithGoogleButton.jsx
@@ -0,0 +1,32 @@
+import googleIconSVG from "../../assets/icons/google.svg";
+import { Button } from "@mui/material";
+import { PropTypes } from "prop-types";
+
+const WithGoogleButton = ({ text }) => {
+    const googleIconHeight = "30px";
+    const googleIcon = <img src={googleIconSVG} height={googleIconHeight} />;
+
+    return (
+        <Button
+            variant="outlined"
+            startIcon={googleIcon}
+            sx={{
+                fontFamily: "Montserrat",
+                fontWeight: "500",
+                textTransform: "none",
+                fontSize: "16px",
+                my: "8px",
+                px: "1.75rem",
+                py: "4px",
+            }}
+        >
+            {text} with Google
+        </Button>
+    );
+};
+
+WithGoogleButton.propTypes = {
+    text: PropTypes.string.isRequired,
+};
+
+export default WithGoogleButton;
