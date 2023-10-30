8f1669fd31335eb7cc95968c040e4d041c7668ba
kcrusselluniversity <kcrussell.university@gmail.com>
2023-10-31 08:17:17 +1100

    Refactor style object into constant

--- src/components/ui/LinkButton.jsx
+++ src/components/ui/LinkButton.jsx
@@ -1,27 +1,16 @@
 import { Button } from "@mui/material";
 import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
-import { accentColor } from "../../data/constants";
+import { CTAButtonStyle } from "../../data/constants";
 
 const LinkButton = ({ text, className, to }) => {
-    const linkButtonStyle = {
-        bgcolor: accentColor,
-        fontFamily: "Montserrat",
-        textTransform: "none",
-        fontSize: "16px",
-        fontWeight: "600",
-        my: "8px",
-        px: "1.75rem",
-        py: "4px",
-    };
-
     return (
         <Button
             variant="contained"
             className={className}
             component={Link}
             to={to}
-            sx={linkButtonStyle}
+            sx={CTAButtonStyle}
         >
             {text}
         </Button>
--- src/data/constants.js
+++ src/data/constants.js
@@ -3,3 +3,19 @@ const secondaryColor = "#a8dadc";
 const accentColor = "#4b86aa";
 
 export { dominantColor, secondaryColor, accentColor };
+
+// This constant holds the style used to modify the MUI Button component
+// for all Call To Action buttons such as a Link button to another page,
+// or a 'Log in' button
+const CTAButtonStyle = {
+    bgcolor: accentColor,
+    fontFamily: "Montserrat",
+    textTransform: "none",
+    fontSize: "16px",
+    fontWeight: "600",
+    my: "8px",
+    px: "1.75rem",
+    py: "4px",
+};
+
+export { CTAButtonStyle };
