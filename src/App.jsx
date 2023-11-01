87a24bd37d720bd4c3cf1e7b7a39d5ccb97683a6
kcrusselluniversity <kcrussell.university@gmail.com>
2023-11-02 06:09:21 +1100

    Refactor firebase config to own module

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,8 @@
 import Router from "./pages/Router";
-import { initializeApp } from "firebase/app";
-
-const firebaseConfig = {
-    apiKey: "AIzaSyBm20LnuKowcEeV0R4UayexrsUvQaAmZr4",
-    authDomain: "gymgraph-ac95d.firebaseapp.com",
-    projectId: "gymgraph-ac95d",
-    storageBucket: "gymgraph-ac95d.appspot.com",
-    messagingSenderId: "569702322733",
-    appId: "1:569702322733:web:912d38d9bb22dd09171ea4"
-  };
-  
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
 
 /**
  * App Component
- * 
+ *
  * The root component of the application, responsible for rendering the main
  * router component that defines the site's navigation.
  */
