7cee9171a8d8b8842f6b7de86fd9d7c0ffa0691c
kcrusselluniversity <kcrussell.university@gmail.com>
2023-11-01 18:56:45 +1100

    Add firebase SDK to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,17 @@
 import Router from "./pages/Router";
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBm20LnuKowcEeV0R4UayexrsUvQaAmZr4",
+    authDomain: "gymgraph-ac95d.firebaseapp.com",
+    projectId: "gymgraph-ac95d",
+    storageBucket: "gymgraph-ac95d.appspot.com",
+    messagingSenderId: "569702322733",
+    appId: "1:569702322733:web:912d38d9bb22dd09171ea4"
+  };
+  
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 /**
  * App Component
