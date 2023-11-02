7253c98cb9a7bd63dd14e517e98c92f93e9d3f58
kcrusselluniversity <kcrussell.university@gmail.com>
2023-11-02 15:50:19 +1100

    Lower form state to SignInForm component

--- src/pages/Signin/SignInForm.jsx
+++ src/pages/Signin/SignInForm.jsx
@@ -1,27 +1,44 @@
 import { TextField, Button } from "@mui/material";
 import { CTAButtonStyle } from "../../data/constants.js";
 import { shape, string, func } from "prop-types";
+import { useState } from "react";
 
 /**
- * SignInForm Component 
- * 
+ * SignInForm Component
+ *
  * This component provides the HTML form used by the Signin page
- * for the user to sign in with their email and password. 
- * 
- * Props: 
- * @param {Object} formData: This object stores the state of the username and 
+ * for the user to sign in with their email and password.
+ *
+ * Props:
+ * @param {Object} formData: This object stores the state of the username and
  * password input fields.
- * @param {function} handleChange: This function updates the formData state 
+ * @param {function} handleChange: This function updates the formData state
  * everytime the corresponding input field changes.
  * @param {function} handleSubmit: This function handles the submitting of the
  * form
- * 
- * Notes: The formData state is passed down as a prop to allow for better
- * testing.
  */
-const SignInForm = ({ formData, handleChange, handleSubmit }) => {
+const SignInForm = () => {
+    // State to manage signin form data
+    const [formData, setFormData] = useState({
+        email: "",
+        password: "",
+    });
+
+    const handleChange = (e) => {
+        setFormData({ ...formData, [e.target.name]: e.target.value });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    };
+
     return (
-        <form name="signInEmail" className="signInEmail" onSubmit={handleSubmit} noValidate>
+        <form
+            name="signInEmail"
+            className="signInEmail"
+            onSubmit={handleSubmit}
+            noValidate
+        >
             <TextField
                 variant="outlined"
                 id="email"
--- src/pages/Signin/Signin.jsx
+++ src/pages/Signin/Signin.jsx
@@ -3,39 +3,22 @@ import Divider from "../../components/ui/Divider";
 import SignInForm from "./SignInForm";
 import WithGoogleButton from "../../components/ui/WithGoogleButton";
 import GymGraphLogo from "../../components/ui/GymGraphLogo";
-import { useState } from "react";
 
 /**
  * Signin page component
- * 
+ *
  * This component provides a user interface for signing in to GymGraph.
  * It includes a form for entering email and password, as well as an option
  * to sign in with Google.
  */
 const Signin = () => {
-    // State to manage signin form data
-    const [formData, setFormData] = useState({
-        email: "",
-        password: "",
-    });
-
-    const handleChange = (e) => {
-        setFormData({ ...formData, [e.target.name]: e.target.value });
-    };
-
-    const handleSubmit = () => {};
-
     return (
         <div className="signInPage">
             <GymGraphLogo />
             <div className="signInPage__content">
                 <h1>Login to GymGraph</h1>
                 <div className="content__signInContainer">
-                    <SignInForm
-                        formData={formData}
-                        handleChange={handleChange}
-                        handleSubmit={handleSubmit}
-                    />
+                    <SignInForm />
                     <Divider direction="vertical" text="or" />
                     <WithGoogleButton text="Continue" />
                 </div>
