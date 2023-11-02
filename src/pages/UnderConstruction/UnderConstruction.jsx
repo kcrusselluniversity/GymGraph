cdb0a9b2f4e658974400301cc7fc30daf537e814
kcrusselluniversity <kcrussell.university@gmail.com>
2023-11-02 13:03:42 +1100

    Create UnderConstruction page

--- src/pages/UnderConstruction/UnderConstruction.jsx
+++ src/pages/UnderConstruction/UnderConstruction.jsx
@@ -1,5 +1,21 @@
+import GymGraphLogo from "../../components/GymGraphLogo";
+import "./underConstruction.css";
+import underConstructionImage from "../../assets/images/under_construction_image_compressed.png";
+
 const UnderConstruction = () => {
-    return <h1>Under construction ...</h1>;
+    return (
+        <div className="underConstruction">
+            <GymGraphLogo />
+            <main className="content">
+                <h1>Sorry, this page is still under construction</h1>
+                <img
+                    className="content__image"
+                    src={underConstructionImage}
+                    alt="page under construction"
+                />
+            </main>
+        </div>
+    );
 };
 
 export default UnderConstruction;
