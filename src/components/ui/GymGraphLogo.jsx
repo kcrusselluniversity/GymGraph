856d6ce6ede38c2e2533b9d4dd71b1030425e8c4
kcrusselluniversity <kcrussell.university@gmail.com>
2023-11-02 13:21:08 +1100

    Update component directory

---
+++ src/components/ui/GymGraphLogo.jsx
@@ -0,0 +1,11 @@
+import logo from "../../assets/Logo.svg"
+
+const GymGraphLogo = () => {
+    return (
+        <header>
+            <img src={logo} alt="GymGraph" className="gymGraphLogo" />
+        </header>
+    );
+};
+
+export default GymGraphLogo
