3c3b7cfaebfba519c148b87fe806d6ca59aac5eb
kcrusselluniversity <kcrussell.university@gmail.com>
2023-11-03 09:55:23 +1100

    Add config for test coverage with v8

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  test: {
-    environment: 'jsdom',
-    setupFiles: "./src/utils/testingUtils/testSetup.js",
-  },
-})
+    plugins: [react()],
+    test: {
+        environment: "jsdom",
+        setupFiles: "./src/utils/testingUtils/testSetup.js",
+        coverage: {
+            provider: "v8",
+        },
+    },
+});
