3cf046ed072a843733a2779fb224d687f0bbd6b3
kcrusselluniversity <kcrussell.university@gmail.com>
2023-11-02 13:04:34 +1100

    Add additional route for "underConstruction"

--- src/pages/Router.jsx
+++ src/pages/Router.jsx
@@ -8,16 +8,16 @@ import Metrics from "./Metrics";
 import Profile from "./Profile";
 import Dashboard from "./Dashboard";
 import Error from "./Error";
-import UnderConstruction from "./UnderConstruction"
+import UnderConstruction from "./UnderConstruction";
 import UserPageLayout from "./UserPageLayout";
 
 /**
  * Router component
- * 
+ *
  * The `Router` component is responsible for defining the routes of the site
  * using React Router. It organizes the site's navigation structure and renders
  * the appropriate components for each route.
- * 
+ *
  * Creating a separate component to house the routes results in more
  * readable code.
  */
@@ -35,6 +35,7 @@ const Router = () => {
                 <Route path="profile" element={<Profile />} />
                 <Route path="settings" element={<UnderConstruction />} />
             </Route>
+            <Route path="underConstruction" element={<UnderConstruction />} />
             <Route path="*" element={<Error />} />
         </Routes>
     );
