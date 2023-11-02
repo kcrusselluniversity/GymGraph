e5287abd52d94bad18ba31b04a3b37bb04d68f24
kcrusselluniversity <kcrussell.university@gmail.com>
2023-11-03 08:18:58 +1100

    Write additional tests for validateEmail function

--- src/utils/__tests__/validateEmail.test.js
+++ src/utils/__tests__/validateEmail.test.js
@@ -2,6 +2,10 @@ import { describe, it, expect } from "vitest";
 import validateEmail from "../formUtils/validateEmail";
 
 describe("validateEmail tests", () => {
+    it("returns error message when email is empty", () => {
+        expect(validateEmail("")).not.toBeNull();
+    });
+
     it("returns an error message for invalid email addresses", () => {
         expect(validateEmail(" notvalid@domain.com")).not.toBeNull();
         expect(validateEmail("not a valid email")).not.toBeNull();
