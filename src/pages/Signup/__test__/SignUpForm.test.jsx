c532d4b9416bdbd8af02be0efc99cba4c5e85723
kcrusselluniversity <kcrussell.university@gmail.com>
2023-11-04 09:03:04 +1100

    Write tests for password and confirmPassword input

--- src/pages/Signup/__test__/SignUpForm.test.jsx
+++ src/pages/Signup/__test__/SignUpForm.test.jsx
@@ -54,14 +54,16 @@ describe("First name input tests", () => {
         await user.type(input, "John");
         expect(input.value).toBe("John");
     });
-    
+
     it("shows error message with incorrect user input", async () => {
         const user = userEvent.setup();
         await user.type(input, "1John");
         await user.tab();
-        expect(screen.getByText(/only alphabetic characters allowed/i)).toBeInTheDocument();
+        expect(
+            screen.getByText(/only alphabetic characters allowed/i)
+        ).toBeInTheDocument();
     });
- 
+
     it("shows no error message with correct user input", async () => {
         const user = userEvent.setup();
         await user.type(input, "John");
@@ -99,7 +101,9 @@ describe("Last name input tests", () => {
         const user = userEvent.setup();
         await user.type(input, "1Smith");
         await user.tab();
-        expect(screen.getByText(/only alphabetic characters allowed/i)).toBeInTheDocument();
+        expect(
+            screen.getByText(/only alphabetic characters allowed/i)
+        ).toBeInTheDocument();
     });
 
     it("shows no error message with correct user input", async () => {
@@ -139,6 +143,94 @@ describe("Email address input tests", () => {
         const user = userEvent.setup();
         await user.type(input, "notanemail");
         await user.tab();
-        expect(screen.getByText(/must be a valid email address/i)).toBeInTheDocument();
+        expect(
+            screen.getByText(/must be a valid email address/i)
+        ).toBeInTheDocument();
+    });
+});
+
+describe("password input tests", () => {
+    let input;
+
+    beforeEach(() => {
+        input = screen.getByLabelText("Password");
+    });
+
+    it("renders with initial value set to an empty string", () => {
+        expect(input.value).toBe("");
+    });
+
+    it("renders with correct placeholder", () => {
+        const placeholder = screen.getByLabelText("Password");
+
+        expect(placeholder).toBeInTheDocument();
+    });
+
+    it("hides the password fields input characters", () => {
+        expect(input).toHaveAttribute("type", "password");
+    });
+
+    it("updates with the correct user input", async () => {
+        const user = userEvent.setup();
+        await user.type(input, "password");
+        expect(input.value).toBe("password");
+    });
+
+    it("shows error message with incorrect user input", async () => {
+        const user = userEvent.setup();
+        await user.type(input, "1");
+        await user.tab();
+        expect(
+            screen.getByText(
+                /password must contain 1 numeric character, 1 captial letter and be at least 8 characters long/i
+            )
+        ).toBeInTheDocument();
+    });
+});
+
+describe.only("confirm password input tests", () => {
+    let input;
+
+    beforeEach(() => {
+        input = screen.getByLabelText("Confirm Password");
+    });
+
+    it("renders with initial value set to an empty string", () => {
+        expect(input.value).toBe("");
+    });
+
+    it("renders with correct placeholder", () => {
+        const placeholder = screen.getByLabelText("Confirm Password");
+        expect(placeholder).toBeInTheDocument();
+    });
+
+    it("hides the confirm password fields input characters", () => {
+        expect(input).toHaveAttribute("type", "password");
+    });
+
+    it("updates with the correct user input", async () => {
+        const user = userEvent.setup();
+        await user.type(input, "confirmPassword");
+        expect(input.value).toBe("confirmPassword");
+    });
+
+    it("shows error message when password and confirmPassword fields dont match", async () => {
+        const user = userEvent.setup();
+        const passwordField = screen.getByLabelText("Password");
+
+        await user.type(passwordField, "password");
+        await user.type(input, "1");
+        await user.tab();
+        expect(screen.getByText(/passwords must match/i)).toBeInTheDocument();
+    });
+
+    it("doesnt show error message when password and confirmPassword fields match", async () => {
+        const user = userEvent.setup();
+        const passwordField = screen.getByLabelText("Password");
+
+        await user.type(passwordField, "password");
+        await user.type(input, "password");
+        await user.tab();
+        expect(screen.queryByText(/passwords must match/i)).not.toBeInTheDocument();
     });
 });
