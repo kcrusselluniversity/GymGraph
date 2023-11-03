d3e5c12ccfa4812ef0e2ebf22a41272841ac9220
kcrusselluniversity <kcrussell.university@gmail.com>
2023-11-04 08:55:07 +1100

    Update confirm password validation logic

--- src/utils/__tests__/validateConfirmPassword.test.js
+++ src/utils/__tests__/validateConfirmPassword.test.js
@@ -2,14 +2,6 @@ import { describe, it, expect } from "vitest";
 import validateConfirmPassword from "../formUtils/validateConfirmPassword";
 
 describe("validateConfirmPassword tests", () => {
-    it("returns error message if the first password field is empty", () => {
-        expect(validateConfirmPassword("", "validPassword!1")).not.toBeNull();
-    });
-
-    it("returns an error message if the first password field is not valid", () => {
-        expect(validateConfirmPassword("password", "")).not.toBeNull();
-    });
-
     it("returns an error message if the two passwords don't match", () => {
         expect(
             validateConfirmPassword("Password1!", "Password2!")
