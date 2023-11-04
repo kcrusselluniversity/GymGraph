22f60112fd4cf5e0655e8ffddf0788e788e7c846
kcrusselluniversity <kcrussell.university@gmail.com>
2023-11-04 11:20:40 +1100

    Test routing

--- src/pages/Signin/__test__/Signin.test.jsx
+++ src/pages/Signin/__test__/Signin.test.jsx
@@ -1,17 +1,44 @@
-import { describe, it, expect, beforeEach } from "vitest";
+import { describe, it, expect } from "vitest";
 import { render, screen } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
 import Signin from "../Signin";
+import userEvent from "@testing-library/user-event";
+import App from '../../../App';
 
-beforeEach(() => {
-    render(<Signin />)
-})
 describe("Signin page", () => {
     it("renders without crashing", () => {
-        expect(screen.getByRole("heading").textContent).toBe("Login to GymGraph")
-    })
+        render(<Signin />, { wrapper: MemoryRouter })
+        
+        expect(screen.getByRole("heading").textContent).toBe(
+            "Login to GymGraph"
+        );
+    });
 
     it("renders logo", () => {
+        render(<Signin />, { wrapper: MemoryRouter })
         const logo = screen.getByRole("img", { name: "GymGraph" });
         expect(logo).toBeInTheDocument();
     });
-})
+
+    it("renders logo with correct route", () => {
+        render(<Signin />, { wrapper: MemoryRouter })
+        const logo = screen.getByAltText("GymGraph");
+        const link = logo.closest("a");
+
+        expect(link).toHaveAttribute('href', '/')
+    });
+
+    it("routes to the landing page when logo clicked", async () => {
+        const user = userEvent.setup();
+
+        render(<MemoryRouter initialEntries={["/signin"]}>
+            <App />
+        </MemoryRouter>)
+
+        const logo = screen.getByAltText("GymGraph");
+        const link = logo.closest('a');
+
+        await user.click(link);
+        expect(screen.getByText(/discover strength unleashed/i)).toBeInTheDocument()
+    })
+});
