d00bfe1b3e2383587a7b429e96d3ab6b17ccace7
kcrusselluniversity <kcrussell.university@gmail.com>
2023-11-04 11:50:26 +1100

    Link logo back to landing page

--- src/pages/UnderConstruction/UnderConstruction.jsx
+++ src/pages/UnderConstruction/UnderConstruction.jsx
@@ -1,12 +1,15 @@
+import { Link } from "react-router-dom";
 import GymGraphLogo from "../../components/ui/GymGraphLogo";
-import "./underConstruction.css";
 import underConstructionImage from "../../assets/images/under_construction_image_compressed.png";
+import "./underConstruction.css";
 
 const UnderConstruction = () => {
     return (
         <div className="underConstruction">
             <header>
-                <GymGraphLogo />
+                <Link to="/">
+                    <GymGraphLogo />
+                </Link>
             </header>
             <main className="content">
                 <h1>Sorry, this page is still under construction</h1>
