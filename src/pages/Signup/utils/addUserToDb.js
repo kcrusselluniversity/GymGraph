d371d0c4ac67529ee05217bbdf094b4da72f0d79
kcrusselluniversity <kcrussell.university@gmail.com>
2023-11-05 07:37:32 +1100

    Create function to add user to firestore db

---
+++ src/pages/Signup/utils/addUserToDb.js
@@ -0,0 +1,10 @@
+import { collection, addDoc } from "firebase/firestore";
+import { db } from "../../../config/firebase";
+
+export const addUserToDb = async (newUser) => {
+    try {
+        await addDoc(collection(db, "users"), newUser)
+    } catch (err) {
+        console.error(err);
+    }
+};
