9001b878c3bb7b0321499ed92c89fefb69aff6b9
kcrusselluniversity <kcrussell.university@gmail.com>
2023-11-04 14:17:17 +1100

    Add firebase auth

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
     apiKey: "AIzaSyBm20LnuKowcEeV0R4UayexrsUvQaAmZr4",
@@ -6,8 +7,11 @@ const firebaseConfig = {
     projectId: "gymgraph-ac95d",
     storageBucket: "gymgraph-ac95d.appspot.com",
     messagingSenderId: "569702322733",
-    appId: "1:569702322733:web:912d38d9bb22dd09171ea4"
-  };
+    appId: "1:569702322733:web:912d38d9bb22dd09171ea4",
+};
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
+
+// Initialize Firebase Authentication and get a reference to the service
+export const auth = getAuth(app);
