584f49acbdbbaecabf1fdaf4edccb1345c77c419
kcrusselluniversity <kcrussell.university@gmail.com>
2023-11-06 12:22:09 +1100

    Refactor tests given LogoutButton refactor

--- src/components/ui/__test__/LogoutButton.test.jsx
+++ src/components/ui/__test__/LogoutButton.test.jsx
@@ -1,21 +1,43 @@
-import { describe, it, expect, beforeEach } from "vitest";
+import { describe, it, expect, vi, afterEach } from "vitest";
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import LogoutButton from "../LogoutButton";
 import { MemoryRouter } from "react-router-dom";
 
-describe("LogoutButton component tests", () => {
-    beforeEach(() => {
-        render(<LogoutButton />, { wrapper: MemoryRouter });
-    });
+const mockNavigate = vi.fn();
 
+vi.mock("react-router-dom", async () => {
+    const reactRouterLibrary = await vi.importActual("react-router-dom");
+    return {
+        ...reactRouterLibrary,
+        useNavigate: vi.fn(() => mockNavigate),
+    };
+});
+
+afterEach(vi.restoreAllMocks);
+
+describe("LogoutButton component tests", () => {
     it("renders without crashing", () => {
-        const logoutButton = screen.getByRole("link", { name: /log out/i });
+        render(<LogoutButton />, { wrapper: MemoryRouter });
+
+        const logoutButton = screen.getByRole("button", { name: /log out/i });
+        
         expect(logoutButton).toBeInTheDocument();
     });
 
-    it("links to the landing page", () => {
-        const logoutButton = screen.getByRole("link", { name: /log out/i });
-        expect(logoutButton).toHaveAttribute('href', '/')
+    it("redirects to root route after click", async () => {
+        const user = userEvent.setup();
+        render(<LogoutButton />, { wrapper: MemoryRouter });
+
+        const logoutButton = screen.getByRole("button", { name: /log out/i });
+        await user.click(logoutButton);
+        
+        expect(mockNavigate).toBeCalledWith("/");
+    });
+
+    it("doesn't call navigate when the button is not clicked", () => {
+        render(<LogoutButton />, { wrapper: MemoryRouter });
+
+        expect(mockNavigate).not.toHaveBeenCalled();
     });
 });
