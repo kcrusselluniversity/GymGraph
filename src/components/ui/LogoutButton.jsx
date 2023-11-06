e1bf43c6429a7bda04c4f0c6142c1107c9289ab6
kcrusselluniversity <kcrussell.university@gmail.com>
2023-11-06 13:14:09 +1100

    Add firebase signout functionality

--- src/components/ui/LogoutButton.jsx
+++ src/components/ui/LogoutButton.jsx
@@ -1,12 +1,20 @@
 import { Button } from "@mui/material";
 import { useNavigate } from "react-router-dom";
+import { signOut } from "firebase/auth";
+import { auth } from "../../config/firebase";
 
 const LogoutButton = () => {
     const navigate = useNavigate();
 
-    const handleClick = () => {
-        navigate("/")
-    }
+    const handleClick = async () => {
+        try {
+            await signOut(auth);
+        } catch (err) {
+            console.error(err);
+        }
+
+        navigate("/");
+    };
 
     return <Button onClick={handleClick}>Log Out</Button>;
 };
