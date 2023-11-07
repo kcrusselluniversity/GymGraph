505e1f2e531d3b1706bfd54407925c443fdcc3ef
kcrusselluniversity <kcrussell.university@gmail.com>
2023-11-07 15:40:46 +1100

    Add loading state

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -1,8 +1,8 @@
 import { useState, useEffect } from "react";
 import { onAuthStateChanged } from "firebase/auth";
-import AuthContext from "./authContext";
+import { AuthContext } from "./authContext";
 import { auth } from "../config/firebase";
-import { object } from "prop-types";
+import { node } from "prop-types";
 
 /**
  * Context Provider to share global state of user
@@ -12,22 +12,24 @@ import { object } from "prop-types";
  */
 const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null);
+    const [isLoading, setisLoading] = useState(true);
 
     useEffect(() => {
         // Set up firebase auth listener on mount
         const unsubscribe = onAuthStateChanged(auth, (user) => {
             setUser(user);
+            setisLoading(false);
         });
 
         // Clean up listener on unmount
         return unsubscribe;
     }, []);
 
-    return <AuthContext.Provider value={user}>{children}</AuthContext.Provider>;
+    return <AuthContext.Provider value={{user, isLoading}}>{children}</AuthContext.Provider>;
 };
 
 AuthProvider.propTypes = {
-    children: object,
+    children: node,
 };
 
 export default AuthProvider;
