4f1803f1adaed0035d940b5b5786f28bd2e7861d
kcrusselluniversity <kcrussell.university@gmail.com>
2023-11-07 15:41:53 +1100

    Refactor AuthContext to standard export

--- src/context/authContext.js
+++ src/context/authContext.js
@@ -1,6 +1,4 @@
 import { createContext } from "react";
 
-// Create context for user authentication 
-const AuthContext = createContext(null);
-
-export default AuthContext;
+// Create context for user authentication
+export const AuthContext = createContext(null);
