a073254df91f7fb9eec4fb57280990dc4c045c92
kcrusselluniversity <kcrussell.university@gmail.com>
2023-11-07 15:42:56 +1100

    Move AuthProvider up the tree

--- src/main.jsx
+++ src/main.jsx
@@ -7,10 +7,10 @@ import "./assets/global.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <BrowserRouter>
-            <AuthProvider>
+        <AuthProvider>
+            <BrowserRouter>
                 <App />
-            </AuthProvider>
-        </BrowserRouter>
+            </BrowserRouter>
+        </AuthProvider>
     </React.StrictMode>
 );
