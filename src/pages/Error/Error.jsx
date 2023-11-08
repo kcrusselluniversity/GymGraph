932acebcbab8ce3a297e9dd59fe2a66b096092ba
kcrusselluniversity <kcrussell.university@gmail.com>
2023-11-08 13:49:48 +1100

    Update content of Error page

--- src/pages/Error/Error.jsx
+++ src/pages/Error/Error.jsx
@@ -1,7 +1,33 @@
-const Error = () => {
-    return <>
-        <h1>404 Error: Page not found</h1>
-    </>
-}
+import { Link } from "react-router-dom";
+import GymGraphLogo from "../../components/ui/GymGraphLogo";
+import "./Error.css";
+import heroImage from "../../assets/images/404_hero_image_compressed.png";
 
-export default Error
+const Error = () => {
+    return (
+        <div className="errorPage">
+            <header>
+                <Link to="/">
+                    <GymGraphLogo />
+                </Link>
+            </header>
+            <div className="errorPage__content">
+                <div className="content__text">
+                    <h1>Gym Fail!</h1>
+                    <h3>
+                        This page decided not to work out today. While it&apos;s
+                        taking a rest day, you might want to head back and choose a
+                        different exercise.
+                    </h3>
+                </div>
+                <img
+                    className="content__heroImage"
+                    src={heroImage}
+                    alt="404 error image"
+                />
+            </div>
+        </div>
+    );
+};
+
+export default Error;
