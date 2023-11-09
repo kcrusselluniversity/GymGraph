7569888998b12e67df766f406c12b5daf3452981
kcrusselluniversity <kcrussell.university@gmail.com>
2023-11-09 19:16:18 +1100

    Add icon svgs to dashboard links

--- src/components/ui/NavBar.jsx
+++ src/components/ui/NavBar.jsx
@@ -1,6 +1,13 @@
 import { NavLink, Link } from "react-router-dom";
+import { ReactSVG } from "react-svg";
 import LogoutButton from "./LogoutButton";
 import GymGraphLogo from "./GymGraphLogo";
+import DashboardIcon from "../../assets/icons/Dashboard_Icon.svg";
+import WorkoutIcon from "../../assets/icons/Workout_Icon.svg";
+import HistoryIcon from "../../assets/icons/History_Icon.svg";
+import MetricsIcon from "../../assets/icons/Metrics_Icon.svg";
+import ProfileIcon from "../../assets/icons/Profile_Icon.svg";
+import SettingsIcon from "../../assets/icons/Settings_Icon.svg";
 import "./NavBar.css";
 
 const NavBar = () => {
@@ -11,22 +18,28 @@ const NavBar = () => {
             </Link>
             <div className="navbar__links">
                 <NavLink className="link" to="/user/dashboard">
-                    Dashboard
+                    <ReactSVG src={DashboardIcon} className="links__icon" />
+                    <p>Dashboard</p>
                 </NavLink>
                 <NavLink className="link" to="/user/workout">
-                    Workout
+                    <ReactSVG src={WorkoutIcon} className="links__icon" />
+                    <p>Workout</p>
                 </NavLink>
                 <NavLink className="link" to="/user/history">
-                    History
+                    <ReactSVG src={HistoryIcon} className="links__icon" />
+                    <p>History</p>
                 </NavLink>
                 <NavLink className="link" to="/user/metrics">
-                    Metrics
+                    <ReactSVG src={MetricsIcon} className="links__icon" />
+                    <p>Metrics</p>
                 </NavLink>
                 <NavLink className="link" to="/user/profile">
-                    Profile
+                    <ReactSVG src={ProfileIcon} className="links__icon" />
+                    <p>Profile</p>
                 </NavLink>
                 <NavLink className="link" to="/user/settings">
-                    Settings
+                    <ReactSVG src={SettingsIcon} className="links__icon" />
+                    <p>Settings</p>
                 </NavLink>
             </div>
             <LogoutButton />
