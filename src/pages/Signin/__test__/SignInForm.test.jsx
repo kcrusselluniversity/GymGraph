57e45acb3cdbb009b711c4018fe9515bb0c7eb7d
kcrusselluniversity <kcrussell.university@gmail.com>
2023-11-12 11:33:02 +1100

    Add form submission rendering tests

--- src/pages/Signin/__test__/SignInForm.test.jsx
+++ src/pages/Signin/__test__/SignInForm.test.jsx
@@ -1,14 +1,15 @@
-import { describe, it, expect, beforeEach } from "vitest";
-import { render, screen } from "@testing-library/react";
+import { describe, it, expect, beforeEach, vi } from "vitest";
+import { render, screen, waitFor } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import SignInForm from "../SignInForm";
 import { MemoryRouter } from "react-router-dom";
-
-beforeEach(() => {
-    render(<SignInForm />, {wrapper: MemoryRouter})
-});
+import handleSignIn from "../utils/handleSignIn";
 
 describe("Initial render tests", () => {
+    beforeEach(() => {
+        render(<SignInForm />, { wrapper: MemoryRouter });
+    });
+
     it("renders without crashing", () => {
         expect(
             screen.getByRole("button", { name: "Log In" })
@@ -29,13 +30,17 @@ describe("Initial render tests", () => {
 
         const passwordField = screen.getByLabelText("Password");
         expect(passwordField.value).toBe("");
-    })
+    });
 });
 
 describe("Form input tests", () => {
+    beforeEach(() => {
+        render(<SignInForm />, { wrapper: MemoryRouter });
+    });
+
     it("handles input being typed into email form field", async () => {
         const emailField = screen.getByLabelText("Email Address");
-        
+
         const user = userEvent.setup();
         await user.type(emailField, "example@gmail.com");
 
@@ -48,25 +53,60 @@ describe("Form input tests", () => {
         const user = userEvent.setup();
         await user.type(passwordField, "12345");
 
-        expect(passwordField.value).toBe("12345")
+        expect(passwordField.value).toBe("12345");
     });
 
     it("hides the password fields input characters", () => {
         const passwordField = screen.getByLabelText("Password");
 
-        expect(passwordField).toHaveAttribute('type', 'password')
-    })
+        expect(passwordField).toHaveAttribute("type", "password");
+    });
 });
 
 describe("Form submission tests", async () => {
-    it("calls the handleSubmit function when the form is submitted", async () => {
-        const emailField = screen.getByLabelText("Email Address");
-        const passwordField = screen.getByLabelText("Password");
-        const submitButton = screen.getByRole("button", { name: /log in/i})
+    vi.mock("../utils/handleSignIn");
+
+    it("calls the handleSignIn function when the form is submitted", async () => {
+        handleSignIn.mockImplementation((e) => {
+            e.preventDefault();
+        });
 
         const user = userEvent.setup();
+        render(<SignInForm />, { wrapper: MemoryRouter });
+
+        const emailField = screen.getByLabelText("Email Address");
+        const passwordField = screen.getByLabelText("Password");
+        const submitButton = screen.getByRole("button", { name: /log in/i });
+
         await user.type(emailField, "example@gmail.com");
         await user.type(passwordField, "password");
-        await user.click(submitButton);    
+        await user.click(submitButton);
+
+        expect(handleSignIn).toHaveBeenCalledTimes(1);
+    });
+
+    it("renders an error message if the input is invalid", async () => {
+        handleSignIn.mockImplementation(
+            (e, formData, setFormSubmissionError) => {
+                e.preventDefault();
+                setFormSubmissionError("Test Error Message");
+            }
+        );
+
+        const user = userEvent.setup();
+        render(<SignInForm />, { wrapper: MemoryRouter });
+
+        const emailField = screen.getByLabelText("Email Address");
+        const passwordField = screen.getByLabelText("Password");
+        const submitButton = screen.getByRole("button", { name: /log in/i });
+
+        await user.type(emailField, "email@email.com");
+        await user.type(passwordField, "PasswordNumber1");
+        await user.click(submitButton);
+
+        await waitFor(() => {
+            const errorElement = screen.getByTestId("errorMessage");
+            expect(errorElement).toBeInTheDocument();
+        });
     });
 });
