433f186f9689b407735ddc8cabfab103bacc709e
kcrusselluniversity <kcrussell.university@gmail.com>
2023-11-12 16:34:21 +1100

    Add tests for newly added onClick prop

--- src/components/ui/__test__/WithGoogleButton.test.jsx
+++ src/components/ui/__test__/WithGoogleButton.test.jsx
@@ -1,5 +1,6 @@
 import { vi, it, expect, describe } from "vitest";
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import WithGoogleButton from "../WithGoogleButton";
 
 describe("WithGoogleButton component", () => {
@@ -13,6 +14,18 @@ describe("WithGoogleButton component", () => {
         render(<WithGoogleButton text="Test" />);
         const iconAltText = screen.getByAltText("Google Icon");
         expect(iconAltText).toBeInTheDocument();
+    });
+
+    it("receives correct function given onClick prop", async () => {
+        const handleClickFunction = vi.fn();
+        const user = userEvent.setup();
+
+        render(<WithGoogleButton text="Continue" onClick={handleClickFunction}/>);
+        const button = screen.getByRole("button", { name: /Continue/i });
+        
+        await user.click(button);
+
+        expect(handleClickFunction).toBeCalledTimes(1);
     });
 
     it("displays the correct text given text prop", () => {
