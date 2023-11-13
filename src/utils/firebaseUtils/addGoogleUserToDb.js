ccd494c0f047b0899a8dd49d3a899d77d3858bbe
kcrusselluniversity <kcrussell.university@gmail.com>
2023-11-14 08:26:52 +1100

    Add function documentation

--- src/utils/firebaseUtils/addGoogleUserToDb.js
+++ src/utils/firebaseUtils/addGoogleUserToDb.js
@@ -1,6 +1,10 @@
 import { collection, query, where, addDoc, getDocs } from "firebase/firestore";
 import { db } from "../../config/firebase";
 
+/**
+ * Add a user signing up with their Google account to the user database
+ * @param {object} newUser: The user object to add to the database
+ */
 export const addGoogleUserToDb = async (newUser) => {
     try {
         const { email: newUserEmail } = newUser;
