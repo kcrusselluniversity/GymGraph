a2778760469a88b1b4150814b7dd5d16757db10d
kcrusselluniversity <kcrussell.university@gmail.com>
2023-11-15 08:54:32 +1100

    Add responsiveness to component

--- src/components/ui/GymGraphLogo.jsx
+++ src/components/ui/GymGraphLogo.jsx
@@ -1,9 +1,13 @@
-import logo from "../../assets/Logo.svg"
+import { useMediaQuery } from "@mui/material";
+import { NavBarDisplaySmallScreen } from "../../data/constants";
+import logoStandard from "../../assets/Logo.svg";
+import logoSmall from "../../assets/Logo--small.svg";
 
 const GymGraphLogo = () => {
-    return (
-            <img src={logo} alt="GymGraph" className="gymGraphLogo" />
-    );
+    const isSmallScreen = useMediaQuery(`(width <= ${NavBarDisplaySmallScreen})`);
+    const logo = isSmallScreen ? logoSmall : logoStandard;
+
+    return <img src={logo} alt="GymGraph" className="gymGraphLogo" />;
 };
 
-export default GymGraphLogo
+export default GymGraphLogo;
