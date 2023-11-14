85e2e36738210c9f299704920dcc8b91b32115d9
kcrusselluniversity <kcrussell.university@gmail.com>
2023-11-15 08:55:34 +1100

    Add responsiveness to component

--- src/components/ui/NavBar.jsx
+++ src/components/ui/NavBar.jsx
@@ -1,14 +1,19 @@
 import { NavLink, Link } from "react-router-dom";
 import { ReactSVG } from "react-svg";
+import { useMediaQuery } from "@mui/material";
 import LogoutButton from "./LogoutButton";
 import GymGraphLogo from "./GymGraphLogo";
 import DashboardIcon from "../../assets/icons/Dashboard_Icon.svg";
 import WorkoutIcon from "../../assets/icons/Workout_Icon.svg";
 import HistoryIcon from "../../assets/icons/History_Icon.svg";
 import MetricsIcon from "../../assets/icons/Metrics_Icon.svg";
+import { NavBarDisplaySmallScreen } from "../../data/constants.js";
 import "./NavBar.css";
 
 const NavBar = () => {
+    // Determine the screen size based on the screen width
+    const isSmallScreen = useMediaQuery(`(width <= ${NavBarDisplaySmallScreen})`);
+
     return (
         <nav className="navbar">
             <Link to="/user/dashboard">
@@ -17,19 +22,19 @@ const NavBar = () => {
             <div className="navbar__links">
                 <NavLink className="link" to="/user/dashboard">
                     <ReactSVG src={DashboardIcon} className="links__icon" />
-                    <p>Dashboard</p>
+                    {isSmallScreen ? null : <p>Dashboard</p>}
                 </NavLink>
                 <NavLink className="link" to="/user/workout">
                     <ReactSVG src={WorkoutIcon} className="links__icon" />
-                    <p>Workout</p>
+                    {isSmallScreen ? null : <p>Workout</p>}
                 </NavLink>
                 <NavLink className="link" to="/user/history">
                     <ReactSVG src={HistoryIcon} className="links__icon" />
-                    <p>History</p>
+                    {isSmallScreen ? null : <p>History</p>}
                 </NavLink>
                 <NavLink className="link" to="/user/metrics">
                     <ReactSVG src={MetricsIcon} className="links__icon" />
-                    <p>Metrics</p>
+                    {isSmallScreen ? null : <p>Metrics</p>}
                 </NavLink>
             </div>
             <LogoutButton />
