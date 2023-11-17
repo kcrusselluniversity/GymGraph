0511f193278b887d83b349febac983366fe7cb0e
kcrusselluniversity <kcrussell.university@gmail.com>
2023-11-17 11:07:52 +1100

    Format stroke offset to represent a percentage

--- src/components/ui/CircularProgressbar.jsx
+++ src/components/ui/CircularProgressbar.jsx
@@ -1,12 +1,28 @@
-import { node } from "prop-types";
+import { node, number } from "prop-types";
 import "./CircularProgressbar.css";
+import { circularProgressbarCircum } from "../../data/constants";
 
-const CircularProgressbar = ({ children }) => {
+/**
+ * Component to represent the percentage of progress of a desired
+ * quantity in a circular format
+ * @param {object} children: The children prop
+ * @param {number} percentage: The percentage of completion
+ */
+const CircularProgressbar = ({ children, percentage=0 }) => {
     return (
         <div className="circularProgressbar__outerCircle">
             <div className="circularProgressbar__innerCircle">{children}</div>
             <svg className="circularProgressbar__circle" viewBox="0 0 100 100">
-                <circle r="45" strokeWidth="10px" cx="50" cy="50" />
+                <circle
+                    r="45"
+                    strokeWidth="10px"
+                    cx="50"
+                    cy="50"
+                    strokeDasharray={circularProgressbarCircum}
+                    strokeDashoffset={
+                        (100 - percentage)/100 * circularProgressbarCircum
+                    }
+                />
             </svg>
         </div>
     );
@@ -14,6 +30,7 @@ const CircularProgressbar = ({ children }) => {
 
 CircularProgressbar.propTypes = {
     children: node.isRequired,
+    percentage: number,
 };
 
 export default CircularProgressbar;
