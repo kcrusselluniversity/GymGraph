d17f69927b05c44e2b0500ef9aae8f1968287b05
kcrusselluniversity <kcrussell.university@gmail.com>
2023-11-19 09:54:41 +1100

    Add documentation

--- src/hooks/useRestTimer.jsx
+++ src/hooks/useRestTimer.jsx
@@ -6,13 +6,18 @@ import {
 } from "../data/constants";
 
 /**
- * Rest Timer Hook
- * @returns {object} An object containing the require state handling
- * functions
+ * useRestTimer is a custom hook used to manage the state of the Rest Timer 
+ * component. It has state for the time as chosen by the user, the time 
+ * remaining, as well as state to manage when the timer is active and when 
+ * it is finished.
+ * 
+ * @returns {object} An object containing the required state and handling 
+ * functions for the Rest Timer component.
+ * 
  * NOTE: We separate the state from the component so we can have the state
  * passed down to the Rest Timer UI component so that it will persist after
- * the UI component has unmounted (for example if the user jumps to a different
- * page)
+ * the UI component has unmounted (for example if the user goes to a different
+ * page then comes back).
  */
 const useRestTimer = () => {
     const [chosenTime, setChosenTime] = useState(initialRestTime);
@@ -39,12 +44,12 @@ const useRestTimer = () => {
         setChosenTime,
         remainingTime,
         setRemainingTime,
-        handleTimeAdded,
-        handleTimeSubtracted,
         isActive,
         setIsActive,
         isFinished,
         setIsFinished,
+        handleTimeAdded,
+        handleTimeSubtracted,
     };
 };
 
