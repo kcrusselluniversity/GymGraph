c0062b541aa0a566b2501300bb2828079f8bb436
kcrusselluniversity <kcrussell.university@gmail.com>
2023-11-18 14:04:36 +1100

    Add state to RestTimer component

--- src/components/ui/RestTimer.jsx
+++ src/components/ui/RestTimer.jsx
@@ -1,15 +1,93 @@
 import CircularProgressbar from "./CircularProgressbar";
+import { Button } from "@mui/material";
 import { number } from "prop-types";
 import "./RestTimer.css";
+import useCountdown from "../../hooks/useCountdown";
+import useRestTimer from "../../hooks/useRestTimer";
+import {
+    SecondaryButtonStyle,
+    timeAdjustmentInSeconds,
+    initialRestTime,
+} from "../../data/constants";
 
 const RestTimer = ({ percentage, diameter }) => {
+    const {
+        chosenTime,
+        setChosenTime,
+        remainingTime,
+        setRemainingTime,
+        handleTimeAdded,
+        handleTimeSubtracted,
+        isActive,
+        setIsActive,
+        isFinished,
+        setIsFinished,
+    } = useRestTimer();
+
+    // use the Countdown hook
+    useCountdown(
+        isActive,
+        setIsActive,
+        remainingTime,
+        setRemainingTime,
+        isFinished,
+        setIsFinished
+    );
+
+    const handleToggleRestTimerClick = () => {
+        setIsActive((prevState) => !prevState);
+    };
+
+    const handleRestartRestTimerClick = () => {
+        setRemainingTime(initialRestTime);
+        setChosenTime(initialRestTime);
+        setIsFinished(false);
+    };
+
     return (
-        <CircularProgressbar percentage={percentage} diameter={diameter}>
-            <div className="RestTimer__content">
-                <span className="RestTimer__chosenTime">2:30</span>
-                <span className="RestTimer__remainingTime">1:59</span>
+        <div className="RestTimer">
+            <CircularProgressbar percentage={percentage} diameter={diameter}>
+                <div className="RestTimer__content">
+                    <span className="RestTimer__chosenTime">{chosenTime}</span>
+                    <span className="RestTimer__remainingTime">
+                        {remainingTime}
+                    </span>
+                </div>
+            </CircularProgressbar>
+            <div className="RestTimer__timeAdjustmentButtons">
+                <Button
+                    variant="contained"
+                    sx={SecondaryButtonStyle}
+                    onClick={handleTimeSubtracted}
+                >
+                    -{timeAdjustmentInSeconds}
+                </Button>
+                <Button
+                    variant="contained"
+                    sx={SecondaryButtonStyle}
+                    onClick={handleTimeAdded}
+                >
+                    +{timeAdjustmentInSeconds}
+                </Button>
             </div>
-        </CircularProgressbar>
+            {isFinished ? (
+                <Button
+                    variant="contained"
+                    sx={SecondaryButtonStyle}
+                    onClick={handleRestartRestTimerClick}
+                >
+                    Restart
+                </Button>
+            ) : (
+                <Button
+                    variant="contained"
+                    sx={SecondaryButtonStyle}
+                    onClick={handleToggleRestTimerClick}
+                >
+                    {isActive ? "Pause" : "Start"}
+                </Button>
+            )}
+        </div>
     );
 };
 
