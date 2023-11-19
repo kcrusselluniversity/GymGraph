e5578a6eb5e7d6e34b2d69fdab755325bca05a46
kcrusselluniversity <kcrussell.university@gmail.com>
2023-11-19 16:38:49 +1100

    Write util function to format a duration

---
+++ src/utils/secondsToMinutesAndSeconds.js
@@ -0,0 +1,22 @@
+const SECONDS_IN_MINUTES = 60;
+
+/**
+ * Coverts a duration given in seconds into minutes and seconds
+ * i.e. of the form 'M:SS'.
+ * @param {number} durationInSeconds
+ * @returns {string}: String representation of the duration as minutes
+ * and seconds
+ */
+const secondsToMinutesAndSeconds = (durationInSeconds) => {
+    if (durationInSeconds < 0) return;
+    if (typeof durationInSeconds != "number") return;
+
+    const minutes = Math.floor(durationInSeconds / SECONDS_IN_MINUTES);
+    const seconds = String(durationInSeconds - SECONDS_IN_MINUTES * minutes);
+
+    const formattedDuration = `${minutes}:${seconds.padStart(2, "0")}`;
+
+    return formattedDuration;
+};
+
+export default secondsToMinutesAndSeconds;
