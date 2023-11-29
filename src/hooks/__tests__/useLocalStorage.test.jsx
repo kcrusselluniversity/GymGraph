189aefdc5a35e6bb8653add1de15f7b5777e1102
kcrusselluniversity <kcrussell.university@gmail.com>
2023-11-29 11:11:10 +1100

    Refactor useLocalStorage hook to sync with state

--- src/hooks/__tests__/useLocalStorage.test.jsx
+++ src/hooks/__tests__/useLocalStorage.test.jsx
@@ -1,5 +1,5 @@
 import { describe, it, expect, beforeEach, vi } from "vitest";
-import { renderHook } from "@testing-library/react";
+import { renderHook, act } from "@testing-library/react";
 import useLocalStorage from "../useLocalStorage";
 
 // Mock localStorage with an IIFE
@@ -10,7 +10,7 @@ const localStorageMock = (function () {
             if (key == "error") {
                 throw new Error("getItem error");
             }
-            return store[key] || null;
+            return JSON.stringify(store[key]) || null;
         },
         setItem(key, value) {
             if (JSON.parse(value) == "setItemError") {
@@ -27,6 +27,9 @@ const localStorageMock = (function () {
         clear() {
             store = {};
         },
+        setInitialValue(key, value) {
+            store[key] = value;
+        },
     };
 })();
 
@@ -40,79 +43,93 @@ beforeEach(() => localStorageMock.clear());
 beforeEach(vi.restoreAllMocks);
 
 // Define a key to be used for the local storage tests
-const key = "testKey";
+const testKey = "testKey";
+const testValue = "testValue";
 
 describe("local storage hook tests", () => {
-    it("sets the key with the correct value", () => {
-        const { result } = renderHook(() => useLocalStorage(key));
-        result.current.setItem("testValue");
+    it("sets the state with the correct inital value", () => {
+        const { result } = renderHook(() =>
+            useLocalStorage(testKey, testValue)
+        );
+        const [, setValue] = result.current;
 
-        expect(result.current.getItem()).toEqual("testValue");
+        act(() => setValue("newValue"));
+
+        expect(result.current[0]).toBe("newValue");
     });
 
-    it("serializes the value before setting the key", () => {
-        // Spy on JSON.stringify
-        const spy = vi.spyOn(JSON, "stringify");
+    it("sets the state with the value stored in local storage if it already exists", () => {
+        // Set an initial value in local storage
+        localStorageMock.setInitialValue(testKey, testValue);
 
-        const { result } = renderHook(() => useLocalStorage(key));
-        result.current.setItem("testValue");
+        // Render the hook
+        const initialValue = "differentInitialValue";
+        const { result } = renderHook(() =>
+            useLocalStorage(testKey, initialValue)
+        );
 
-        expect(spy).toHaveBeenCalledTimes(1);
+        expect(result.current[0]).toBe(testValue);
+        expect(result.current[0]).not.toBe(initialValue);
+    });
+
+    it("updates the locally stored value when the state is changed", () => {
+        const { result } = renderHook(() =>
+            useLocalStorage(testKey, testValue)
+        );
+        const [, setValue] = result.current;
+
+        const differentValue = "differentInitialValue";
+        act(() => setValue(differentValue))
+
+        expect(localStorageMock.getItem(testKey)).toMatch(differentValue);
+    });
+
+    it("removes the key and value pair correctly", () => {
+        const { result } = renderHook(() =>
+            useLocalStorage(testKey, testValue)
+        );
+        const [, , removeValue] = result.current;
+
+        act(() => removeValue());
+
+        expect(result.current[0]).toBeNull();
+        expect(localStorageMock.getItem(testKey)).toBeNull()
     });
 
     it("logs an error when the local Storage setItem function errors", () => {
         // Spy on console.error
         const spy = vi.spyOn(console, "error").mockImplementation(() => {});
 
-        const { result } = renderHook(() => useLocalStorage(key));
-        result.current.setItem("setItemError");
+        const { result } = renderHook(() =>
+            useLocalStorage(testKey, testValue)
+        );
+        const [, setValue] = result.current;
+
+        act(() => setValue("setItemError"));
 
         expect(spy).toHaveBeenCalled();
-    });
-
-    it("parses the item before returning it from local storage", () => {
-        const { result } = renderHook(() => useLocalStorage(key));
-        result.current.setItem("testValue");
-
-        // Spy on JSON.parse
-        const spy = vi.spyOn(JSON, "parse");
-
-        result.current.getItem();
-        expect(spy).toHaveBeenCalledTimes(1);
-    });
-
-    it("returns undefined if no item is stored in localStorage", () => {
-        const { result } = renderHook(() => useLocalStorage(key));
-
-        const item = result.current.getItem();
-        expect(item).toBeUndefined();
     });
 
     it("logs an error when the local Storage getItem function errors", () => {
         // Spy on console.error
         const spy = vi.spyOn(console, "error").mockImplementation(() => {});
-        const { result } = renderHook(() => useLocalStorage("error"));
-
-        result.current.getItem();
+        renderHook(() => useLocalStorage("error", testValue));
 
         expect(spy).toHaveBeenCalledTimes(1);
     });
 
-    it("removes the key and value pair correctly", () => {
-        const { result } = renderHook(() => useLocalStorage(key));
-        result.current.setItem("testValue");
 
-        result.current.removeItem();
-
-        expect(result.current.getItem()).toBeUndefined();
-    });
 
     it("logs an error when the local Storage removeItem function errors", () => {
         // Spy on console.error
         const spy = vi.spyOn(console, "error").mockImplementation(() => {});
-        const { result } = renderHook(() => useLocalStorage("error"));
 
-        result.current.removeItem();
+        const { result } = renderHook(() =>
+            useLocalStorage("error", testValue)
+        );
+        const [, , removeValue] = result.current;
+
+        removeValue();
 
         expect(spy).toHaveBeenCalledTimes(1);
     });
--- src/hooks/useLocalStorage.jsx
+++ src/hooks/useLocalStorage.jsx
@@ -1,30 +1,39 @@
-const useLocalStorage = (key) => {
-    const setItem = (value) => {
-        try {
-            window.localStorage.setItem(key, JSON.stringify(value));
-        } catch (err) {
-            console.error(err);
-        }
-    };
+import { useState, useEffect } from "react";
 
-    const getItem = () => {
+const useLocalStorage = (key, initialValue) => {
+    const [value, setValue] = useState(() => {
         try {
+            // Initialise state with stored local storage value
+            // (if it already exists)
             const item = window.localStorage.getItem(key);
-            return item ? JSON.parse(item) : undefined;
+            return item ? JSON.parse(item) : initialValue;
+        } catch (err) {
+            console.error(err);
+            return initialValue;
+        }
+    });
+
+    // Update local storage when value changes
+    useEffect(() => {
+        try {
+            value == null
+                ? window.localStorage.removeItem(key)
+                : window.localStorage.setItem(key, JSON.stringify(value));
+        } catch (err) {
+            console.error(err);
+        }
+    }, [key, value]);
+
+    // Function to remove the value from local storage
+    const removeValue = () => {
+        try {
+            setValue(null);
         } catch (err) {
             console.error(err);
         }
     };
 
-    const removeItem = () => {
-        try {
-            window.localStorage.removeItem(key)
-        } catch (err) {
-            console.error(err)
-        }
-    }
-
-    return { setItem, getItem, removeItem };
+    return [value, setValue, removeValue];
 };
 
 export default useLocalStorage;
