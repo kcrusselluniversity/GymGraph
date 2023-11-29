e86739bbd5eaef61e5f74a392d4fb78233322b33
kcrusselluniversity <kcrussell.university@gmail.com>
2023-11-29 20:03:53 +1100

    Clean up code

--- src/pages/Workout/components/StartWorkoutButton.jsx
+++ src/pages/Workout/components/StartWorkoutButton.jsx
@@ -2,17 +2,12 @@ import { Button } from "@mui/material";
 import { PinkCTAButtonStyle } from "../../../data/constants";
 import { func } from "prop-types";
 
-const startWorkoutButtonStyle = {
-    ...PinkCTAButtonStyle,
-    width: "80%",
-};
-
 const StartWorkoutButton = ({ handleClick }) => {
     return (
         <Button
             onClick={handleClick}
             className="startWorkoutButton"
-            sx={startWorkoutButtonStyle}
+            sx={PinkCTAButtonStyle}
         >
             <span>Start Workout</span>
             <span className="addSymbol">+</span>
