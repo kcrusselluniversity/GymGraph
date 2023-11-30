ec90818693d2e5a3b9f3362211d98c6507627f39
kcrusselluniversity <kcrussell.university@gmail.com>
2023-12-01 09:58:08 +1100

    Implement additional components

--- src/pages/Workout/components/ExerciseSession.jsx
+++ src/pages/Workout/components/ExerciseSession.jsx
@@ -1,9 +1,13 @@
 import { string } from "prop-types";
 import AddExerciseButton from "./AddExerciseButton";
+import RestTimerButton from "./RestTimerButton";
+import ExerciseItem from "./ExerciseItem";
 
 const ExerciseSession = (props) => {
     return <div className="exerciseSession">
         <AddExerciseButton />
+        <ExerciseItem />
+        <RestTimerButton />
     </div>;
 };
 
