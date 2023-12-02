f60577909d34cdbf4d083ed5f6335521cf8a90c3
kcrusselluniversity <kcrussell.university@gmail.com>
2023-12-02 14:26:25 +1100

    Add state to workout page

--- src/pages/Workout/Workout.jsx
+++ src/pages/Workout/Workout.jsx
@@ -4,12 +4,18 @@ import ExerciseSession from "./components/ExerciseSession";
 import { v4 as uuid } from "uuid";
 import "./workout.css";
 
+import { TEST_EXERCISE_LIST } from "../../data/constants";
+
 const Workout = () => {
     const [workoutActive, setWorkout, removeWorkout] = useLocalStorage(
         "isWorkoutActive",
         false
     );
     const [sessionId, setSessionId , removeSessionId] = useLocalStorage("sessionId", null);
+    const [exercises, setExercises, removeExercises] = useLocalStorage(
+        uuid(),
+        TEST_EXERCISE_LIST
+    );
 
     const handleClick = () => {
         setWorkout(true);
@@ -22,13 +28,14 @@ const Workout = () => {
                 onClick={() => {
                     removeWorkout();
                     removeSessionId();
+                    removeExercises();
                 }}
             >
                 Remove
             </button>
             <button onClick={() => setWorkout(!workoutActive)}>Toggle</button>
             {workoutActive ? (
-                <ExerciseSession sessionId={sessionId} />
+                <ExerciseSession exercises={exercises} setExercises={setExercises}/>
             ) : (
                 <StartWorkoutButton handleClick={handleClick} />
             )}
