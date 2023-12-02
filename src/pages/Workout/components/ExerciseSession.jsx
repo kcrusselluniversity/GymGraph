e2865b6b1402822c78432935406e7ed75849c902
kcrusselluniversity <kcrussell.university@gmail.com>
2023-12-03 10:15:36 +1100

    Add rest timer modal to component

--- src/pages/Workout/components/ExerciseSession.jsx
+++ src/pages/Workout/components/ExerciseSession.jsx
@@ -1,9 +1,27 @@
-import AddExerciseButton from "./AddExerciseButton";
+import { Dialog } from "@mui/material";
 import RestTimerButton from "./RestTimerButton";
+import RestTimer from "../../../components/ui/RestTimer";
+import AddExerciseButton from "./AddExerciseButton";
 import ExerciseItem from "./ExerciseItem";
 import { PropTypes, func, number, string } from "prop-types";
+import { useState } from "react";
 
+/**
+ * Exercise Session component
+ *
+ * This component renders when the user starts an exercise session.
+ * It is responsible for providing the UI to add exercises and view the
+ * data of the current workout session.
+ *
+ * @param {object} exercises: Object containing the data on the current workout
+ * sessions exercises
+ * @param {function} setExercises: State setter function to update the
+ * exercises object state
+ * @returns {any}
+ */
 const ExerciseSession = ({ exercises, setExercises }) => {
+    const [isRestTimerOpen, setIsRestTimerOpen] = useState(false);
+
     const exerciseItems = exercises?.map((exercise) => {
         return <ExerciseItem key={exercise.name} exercise={exercise} />;
     });
@@ -12,7 +30,15 @@ const ExerciseSession = ({ exercises, setExercises }) => {
         <div className="exerciseSession">
             <AddExerciseButton />
             {exerciseItems}
-            <RestTimerButton />
+            <RestTimerButton
+                handleClick={() => setIsRestTimerOpen(!isRestTimerOpen)}
+            />
+            <Dialog
+                open={isRestTimerOpen}
+                onClose={() => setIsRestTimerOpen(false)}
+            >
+                <RestTimer />
+            </Dialog>
         </div>
     );
 };
