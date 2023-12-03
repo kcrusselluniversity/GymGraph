4d8dab07b0c0fde423ec58a5ba58fc94a486421a
kcrusselluniversity <kcrussell.university@gmail.com>
2023-12-03 16:21:59 +1100

    Write ExerciseModal component

---
+++ src/pages/Workout/components/ExerciseModal.jsx
@@ -0,0 +1,11 @@
+import SearchBar from "../../../components/ui/SearchBar";
+
+const ExerciseModal = () => {
+    return (
+        <div className="ExerciseModal">
+            <SearchBar placeholder="Search exercise" name="exercise" />
+        </div>
+    );
+};
+
+export default ExerciseModal;
