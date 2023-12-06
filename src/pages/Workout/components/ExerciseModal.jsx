08ef246b1c86be4192f3a72701189c1528750bd6
kcrusselluniversity <kcrussell.university@gmail.com>
2023-12-06 11:34:34 +1100

    Add exercise accordion component to modal

--- src/pages/Workout/components/ExerciseModal.jsx
+++ src/pages/Workout/components/ExerciseModal.jsx
@@ -1,9 +1,11 @@
 import SearchBar from "../../../components/ui/SearchBar";
+import ExercisesAccordion from './ExercisesAccordion';
 
 const ExerciseModal = () => {
     return (
         <div className="ExerciseModal">
             <SearchBar placeholder="Search exercise" name="exercise" />
+            <ExercisesAccordion />
         </div>
     );
 };
