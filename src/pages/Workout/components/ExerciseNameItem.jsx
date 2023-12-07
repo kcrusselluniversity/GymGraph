8932850a83dce3eb492188004a825e4a0c5fbbd6
kcrusselluniversity <kcrussell.university@gmail.com>
2023-12-08 09:19:52 +1100

    Add exercise modal state and logic to component

--- src/pages/Workout/components/ExerciseNameItem.jsx
+++ src/pages/Workout/components/ExerciseNameItem.jsx
@@ -1,18 +1,33 @@
 import { string } from "prop-types";
+import { useContext } from "react";
+import { exerciseModalContext } from "../../../context/exerciseModalContext";
 /**
  * ExerciseNameItem component
  *
- * Takes in an exercise name and returns a UI component displaying the exercise
+ * Takes an exercise and returns a UI component displaying the exercise
  */
-const ExerciseNameItem = ({ exerciseName }) => {
+const ExerciseNameItem = ({ uid, exerciseName }) => {
+    const { setSelectedExerciseInfo, setExerciseModalState } =
+        useContext(exerciseModalContext);
+
+    const handleClick = () => {
+        setSelectedExerciseInfo(uid)
+        setExerciseModalState("selected_exercise_info")
+    };
+
     return (
-        <div className="exerciseGroupList__item" data-testid="exerciseNameItem">
+        <div
+            className="exerciseGroupList__item"
+            data-testid="exerciseNameItem"
+            onClick={handleClick}
+        >
             {exerciseName}
         </div>
     );
 };
 
 ExerciseNameItem.propTypes = {
+    uid: string,
     exerciseName: string,
 };
 
