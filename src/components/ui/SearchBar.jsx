c3c9b922749ffc2f4a45c6a2fb75d9dd2dc3e6e9
kcrusselluniversity <kcrussell.university@gmail.com>
2023-12-07 14:43:51 +1100

    Refactor state to be provided by parent level component

--- src/components/ui/SearchBar.jsx
+++ src/components/ui/SearchBar.jsx
@@ -1,26 +1,27 @@
 import { Search } from "@mui/icons-material";
 import { IconButton, TextField } from "@mui/material";
-import { func } from "prop-types";
-import { useState } from "react";
+import { func, PropTypes, string } from "prop-types";
 
 /**
  * SearchBar component
  *
  * @param {function} handleSearch: This function is called with the text input
  * provided by the user.
+ * @param {object} state: This object contains the state value and setter function
+ * for the text field inputs value.
  * @param extraProps: This is an object containing any additional props passed
  * to this component. Intented to be used to pass custom attribute values
  * for the search bars input field.
  */
-const SearchBar = ({ handleSearch, ...extraProps }) => {
-    const [input, setInput] = useState("");
+const SearchBar = ({ handleSearch, state, ...extraProps }) => {
+    const { searchInput, setSearchInput } = state;
 
     const handleSubmit = (e) => {
         e.preventDefault();
+        console.log(searchInput);
         if (handleSearch == undefined) return;
-
-        // Call handleSearch function 
-        handleSearch(input);
+        // Call handleSearch function
+        handleSearch(searchInput);
     };
 
     return (
@@ -30,8 +31,8 @@ const SearchBar = ({ handleSearch, ...extraProps }) => {
                 name={extraProps?.name}
                 id={extraProps?.id}
                 size="small"
-                value={input}
-                onChange={(e) => setInput(e.target.value)}
+                value={searchInput}
+                onChange={(e) => setSearchInput(e.target.value)}
             />
             <IconButton type="submit" aria-label="search">
                 <Search />
@@ -42,6 +43,10 @@ const SearchBar = ({ handleSearch, ...extraProps }) => {
 
 SearchBar.propTypes = {
     handleSearch: func,
+    state: PropTypes.shape({
+        searchInput: string,
+        setSearchInput: func,
+    }),
 };
 
 export default SearchBar;
