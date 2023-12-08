c1bb87db923f95fbc65a11e7c87c7ab578f1f5b4
kcrusselluniversity <kcrussell.university@gmail.com>
2023-12-08 12:49:58 +1100

    Add exercise details to body of component

--- src/pages/Workout/components/ExerciseDetails.jsx
+++ src/pages/Workout/components/ExerciseDetails.jsx
@@ -1,12 +1,14 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { exerciseModalContext } from "../../../context/exerciseModalContext";
 import getExerciseByUid from "../../../utils/getExerciseByUid";
-import { GreyButtonStyle } from "../../../data/constants";
-import { Button } from "@mui/material";
+import { GreyButtonStyle, exerciseGifUrlEndpoint, gifStandardSize } from "../../../data/constants";
+import { Button, Skeleton } from "@mui/material";
 import { ReactSVG } from "react-svg";
 import BackIcon from "../../../assets/icons/Back_Icon.svg";
+import { errorMessage } from "../../../utils/getExerciseByUid";
 
 const ExerciseDetails = () => {
+    const [isGifLoading, setIsGifLoading] = useState(true);
     const { searchInput, selectedExerciseInfo, setExerciseModalState } =
         useContext(exerciseModalContext);
 
@@ -15,7 +17,28 @@ const ExerciseDetails = () => {
         setExerciseModalState(newModalState);
     };
 
+    // Get the exercise object from the exerciseList JSON
     const exercise = getExerciseByUid(selectedExerciseInfo);
+
+    // Display an error message if no exercise is found
+    if (exercise === errorMessage)
+        return (
+            <div className="exerciseDetails">
+                <div className="exerciseDetails__header">
+                    <Button
+                        sx={{ ...GreyButtonStyle, padding: "0px" }}
+                        onClick={handleBackArrowClick}
+                    >
+                        <ReactSVG className="links__icon" src={BackIcon} />
+                    </Button>
+                </div>
+                {errorMessage}
+            </div>
+        );
+
+    // Destructure the exercise object
+    const { exercise: name, muscleGroup, uid } = exercise;
+    const exerciseGifUrl = `${exerciseGifUrlEndpoint}/${uid}.gif`;
 
     return (
         <div className="exerciseDetails">
@@ -32,6 +55,35 @@ const ExerciseDetails = () => {
                 >
                     <ReactSVG className="links__icon" src={BackIcon} />
                 </Button>
+            </div>
+            <div className="exerciseDetails__body">
+                <h2 style={{ textAlign: "center" }}>{name}</h2>
+                <h4>{muscleGroup}</h4>
+                <div
+                    className="gifContainer"
+                    style={{
+                        height: `${gifStandardSize}px`,
+                        width: `${gifStandardSize}px`,
+                    }}
+                >
+                    <img
+                        alt={name}
+                        src={exerciseGifUrl}
+                        onLoad={() => setIsGifLoading(false)}
+                    />
+                    {isGifLoading && (
+                        <Skeleton
+                            className="exerciseDetails__skeleton"
+                            variant="rectangular"
+                            height={gifStandardSize}
+                            width={gifStandardSize}
+                            sx={{
+                                borderRadius: "1rem",
+                                opacity: isGifLoading ? 1 : 0,
+                            }}
+                        />
+                    )}
+                </div>
             </div>
         </div>
     );
