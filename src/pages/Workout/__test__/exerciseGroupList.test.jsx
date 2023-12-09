e9f0bc10d0b88c45b07e6a6c5cb2590e5298157a
kcrusselluniversity <kcrussell.university@gmail.com>
2023-12-09 13:11:52 +1100

    Add context wrapper to fix test bug

--- src/pages/Workout/__test__/exerciseGroupList.test.jsx
+++ src/pages/Workout/__test__/exerciseGroupList.test.jsx
@@ -1,19 +1,20 @@
 import { describe, it, expect } from "vitest";
 import { render, screen } from "@testing-library/react";
 import exerciseGroupList from "../utils/exerciseGroupList";
+import ExerciseModalProvider from '../../../context/ExerciseModalProvider';
 
 describe("exerciseGroupList tests", () => {
     it("renders without crashing given a valid exercise group", () => {
         const validExerciseGroup = "chest";
 
-        render(exerciseGroupList(validExerciseGroup));
+        render(exerciseGroupList(validExerciseGroup), {wrapper: ExerciseModalProvider});
         expect(screen.getAllByText(/chest press/i)).not.toHaveLength(0);
     });
 
     it("renders without crashing given an invalid exercise group", () => {
         const validExerciseGroup = "tenticle";
 
-        render(exerciseGroupList(validExerciseGroup));
+        render(exerciseGroupList(validExerciseGroup), {wrapper: ExerciseModalProvider});
         expect(screen.queryAllByTestId("exerciseNameItem")).toHaveLength(0);
     });
 });
