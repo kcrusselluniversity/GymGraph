7602645bdaaf1542a43aeaebb7b674d00618db06
kcrusselluniversity <kcrussell.university@gmail.com>
2023-12-10 11:15:50 +1100

    Refactor Modal back button to separate reusable component

---
+++ src/components/ui/BackButton.jsx
@@ -0,0 +1,22 @@
+import { GREY_STYLE_BUTTON } from "../../data/constants.js";
+import { Button } from "@mui/material";
+import { ReactSVG } from "react-svg";
+import BackIcon from "../../assets/icons/Back_Icon.svg";
+import { func } from "prop-types";
+
+const BackButton = ({ handleBackArrowClick }) => {
+    return (
+        <Button
+            sx={{ ...GREY_STYLE_BUTTON, padding: "0px" }}
+            onClick={handleBackArrowClick}
+        >
+            <ReactSVG className="links__icon" src={BackIcon} fallback="Back"/>
+        </Button>
+    );
+};
+
+BackButton.propTypes = {
+    handleBackArrowClick: func,
+};
+
+export default BackButton;
