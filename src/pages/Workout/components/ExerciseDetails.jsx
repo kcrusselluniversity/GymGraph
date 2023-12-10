8f50126852c4960bc2d993ab6e0c041455cfe86b
kcrusselluniversity <kcrussell.university@gmail.com>
2023-12-10 11:14:53 +1100

    Add documentation, refactor back btn click handler

--- src/pages/Workout/components/ExerciseDetails.jsx
+++ src/pages/Workout/components/ExerciseDetails.jsx
@@ -10,15 +10,48 @@ import { Button, Skeleton } from "@mui/material";
 import { ReactSVG } from "react-svg";
 import BackIcon from "../../../assets/icons/Back_Icon.svg";
 import { errorMessage } from "../../../utils/getExerciseByUid";
+import BackButton from "../../../components/ui/BackButton";
 
+/**
+ * Exercise Details component
+ *
+ * This component is shown once the user selects an exercise from the
+ * exercise list. It displays the name, muscle group and an animated gif
+ * of the exercise. The user can choose to add the exercise to their session
+ * or go back and choose a different exercise.
+ *
+ * This component can also be reached if the user has chosen an exercise for
+ * their session and wants to see the information about the exercise while
+ * they are currently filling out the session data. In this case the
+ * add exercise button will not be available.
+ */
 const ExerciseDetails = () => {
     const [isGifLoading, setIsGifLoading] = useState(true);
-    const { searchInput, selectedExerciseInfo, setExerciseModalState } =
-        useContext(exerciseModalContext);
+
+    // Deconstruct the required global exercise modal context
+    const {
+        searchInput,
+        selectedExerciseInfo,
+        exerciseAdded,
+        setExerciseAdded,
+        setExerciseModalState,
+    } = useContext(exerciseModalContext);
 
     const handleBackArrowClick = () => {
-        const newModalState = searchInput === "" ? "default" : "user_search";
-        setExerciseModalState(newModalState);
+        // If the user is viewing this component for the exercise details
+        // after they have added the exercise to their session, then the back
+        // button will take them back to the exercise session data componet
+        if (exerciseAdded) {
+            setExerciseModalState("exercise_session_data");
+        } else {
+            const newModalState = searchInput === "" ? "default" : "user_search";
+            setExerciseModalState(newModalState);
+        }
+    };
+
+    const handleAddExerciseClick = () => {
+        setExerciseAdded(exercise);
+        setExerciseModalState("exercise_session_data");
     };
 
     // Get the exercise object from the exerciseList JSON
@@ -47,18 +80,16 @@ const ExerciseDetails = () => {
     return (
         <div className="exerciseDetails">
             <div className="exerciseDetails__header">
-                <Button
-                    sx={GREY_STYLE_BUTTON}
-                    className="exerciseDetails__addExerciseBtn"
-                >
-                    Add Exercise
-                </Button>
-                <Button
-                    sx={{ ...GREY_STYLE_BUTTON, padding: "0px" }}
-                    onClick={handleBackArrowClick}
-                >
-                    <ReactSVG className="links__icon" src={BackIcon} />
-                </Button>
+                {!exerciseAdded && (
+                    <Button
+                        sx={GREY_STYLE_BUTTON}
+                        className="exerciseDetails__addExerciseBtn"
+                        onClick={handleAddExerciseClick}
+                    >
+                        Add Exercise
+                    </Button>
+                )}
+                <BackButton handleBackArrowClick={handleBackArrowClick} />
             </div>
             <div className="exerciseDetails__body">
                 <h2 style={{ textAlign: "center" }}>{name}</h2>
