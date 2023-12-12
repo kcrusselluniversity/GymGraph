25443ecdeadb5a54128f7892c40bb6d26f8831b9
kcrusselluniversity <kcrussell.university@gmail.com>
2023-12-12 18:33:23 +1100

    Add documentation to component

--- src/pages/Workout/components/AddExerciseButton.jsx
+++ src/pages/Workout/components/AddExerciseButton.jsx
@@ -2,6 +2,14 @@ import { Button } from "@mui/material";
 import { GREY_STYLE_BUTTON } from "../../../data/constants";
 import { func } from "prop-types";
 
+/**
+ * Add exercise button component
+ * 
+ * Used inside an exercise session to allow the user to add exercises to their
+ * current workout session.
+ * 
+ * @param {function} handleClick: click handler for exercise button
+ */
 const AddExerciseButton = ({ handleClick }) => {
     return (
         <Button
