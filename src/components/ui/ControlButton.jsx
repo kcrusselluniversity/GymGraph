6e65e9cdc13ff0fc088c68c588fca317dbccae5e
kcrusselluniversity <kcrussell.university@gmail.com>
2023-12-12 14:38:06 +1100

    Add new type to allowed button types

--- src/components/ui/ControlButton.jsx
+++ src/components/ui/ControlButton.jsx
@@ -6,11 +6,13 @@ import AddIcon from "../../assets/icons/Add_Icon.svg";
 import DeleteIcon from "../../assets/icons/Delete_Icon.svg";
 import EditIcon from "../../assets/icons/Edit_Icon.svg";
 import TrashIcon from "../../assets/icons/Trash_Icon.svg";
+import PlusIcon from "../../assets/icons/Plus_Icon.svg";
 
 // A color property is added to each button type to style the button
 // using the MUI color property.
 const buttonTypes = {
-    add: { icon: AddIcon, color: "success" },
+    add: { icon: PlusIcon, color: "success" },
+    done: { icon: AddIcon, color: "success" },
     delete: { icon: DeleteIcon, color: "error" },
     edit: { icon: EditIcon, color: "primary" },
     trash: { icon: TrashIcon, color: "primary" },
