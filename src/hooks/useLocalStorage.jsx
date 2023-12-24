83496c297dc69dd59e64591b2e7f8d569dadf7fc
kcrusselluniversity <kcrussell.university@gmail.com>
2023-12-24 12:00:53 +1100

    Update logic of removeValue function

--- src/hooks/useLocalStorage.jsx
+++ src/hooks/useLocalStorage.jsx
@@ -2,38 +2,28 @@ import { useState, useEffect } from "react";
 
 /**
  * useLocalStorage hook
- * 
+ *
  * This hook wraps around a useState hook in order to store a state value
- * in local storage and keep the locally stored value in sync with the state. 
- * 
- * The motivation for this was to persist data between refreshes for the 
- * application to be more versitile, as well as to be used for storing 
+ * in local storage and keep the locally stored value in sync with the state.
+ *
+ * The motivation for this was to persist data between refreshes for the
+ * application to be more versitile, as well as to be used for storing
  * locally a users Exercise data during a training session.
- * This is more desirable than making a network request with every change 
- * (and thus improving fluidity and user experience). 
- * 
- * When the session is complete then all the data is saved to 
- * the backend database in one single request. 
- * 
+ * This is more desirable than making a network request with every change
+ * (and thus improving fluidity and user experience).
+ *
+ * When the session is complete then all the data is saved to
+ * the backend database in one single request.
+ *
  * @param {string} key: The key to store the value in local storage
  * @param {any} initialValue: The initial value to store
- * @param {object} ClassName: Option parameter to pass to the hook if the 
+ * @param {object} ClassName: Option parameter to pass to the hook if the
  * value stored in local storage is an object created from a class.
- * 
+ *
  * @returns {array}: The array given by [value, setValue, removeValue]
  */
 const useLocalStorage = (key, initialValue, ClassName) => {
     const [value, setValue] = useState(() => {
-        // try {
-        //     // Initialise state with stored local storage value
-        //     // (if it already exists)
-        //     const item = window.localStorage.getItem(key);
-        //     return item ? JSON.parse(item) : initialValue;
-        // } catch (err) {
-        //     console.error(err);
-        //     return initialValue;
-        // }
-
         try {
             const item = window.localStorage.getItem(key);
             if (item === null) return initialValue;
@@ -42,7 +32,7 @@ const useLocalStorage = (key, initialValue, ClassName) => {
             // (if it already exists)
             const parsedItem = JSON.parse(item);
             if (ClassName === undefined) {
-                return parsedItem
+                return parsedItem;
             } else {
                 return new ClassName(parsedItem);
             }
@@ -55,9 +45,7 @@ const useLocalStorage = (key, initialValue, ClassName) => {
     // Update local storage when value changes
     useEffect(() => {
         try {
-            value == null
-                ? window.localStorage.removeItem(key)
-                : window.localStorage.setItem(key, JSON.stringify(value));
+            window.localStorage.setItem(key, JSON.stringify(value));
         } catch (err) {
             console.error(err);
         }
@@ -66,7 +54,8 @@ const useLocalStorage = (key, initialValue, ClassName) => {
     // Function to remove the value from local storage
     const removeValue = () => {
         try {
-            setValue(null);
+            window.localStorage.removeItem(key);
+            setValue(initialValue);
         } catch (err) {
             console.error(err);
         }
