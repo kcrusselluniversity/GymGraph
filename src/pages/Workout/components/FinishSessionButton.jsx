8ddf7636aa87d7d482d5e9b70e845e6c4218466c
kcrusselluniversity <kcrussell.university@gmail.com>
2023-12-24 12:01:38 +1100

    Add state updating on click

--- src/pages/Workout/components/FinishSessionButton.jsx
+++ src/pages/Workout/components/FinishSessionButton.jsx
@@ -1,4 +1,6 @@
+import { useContext } from "react";
 import GreyButton from "../../../components/ui/GreyButton";
+import { exerciseModalContext } from "../../../context/exerciseModalContext";
 
 /**
  * FinishSessionButton component
@@ -8,13 +10,29 @@ import GreyButton from "../../../components/ui/GreyButton";
  * to their database.
  */
 const FinishSessionButton = () => {
+    // Destructure required context state
+    const {
+        setExerciseModalState,
+        removeWorkoutActive,
+        removeSessionStartTime,
+        removeSessionExercises,
+        setSelectedExerciseInfo,
+        setExerciseAdded,
+    } = useContext(exerciseModalContext);
+
     const handleClick = () => {
         // Check if any exercises were added
         // If they have, update the state
-            // Upload data to firestore
+        // Upload data to firestore
         // If not, update the relevant state accordingly
         
-        // Update state to end session
+        // Update state to end session and prepare for next session
+        removeWorkoutActive();
+        removeSessionStartTime();
+        removeSessionExercises();
+        setExerciseModalState("default");
+        setSelectedExerciseInfo(null);
+        setExerciseAdded(null);
     };
 
     return (
