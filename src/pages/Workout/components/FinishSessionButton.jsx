59673caeb18ccb8ffc9bb4dbe006eab78bc5e27b
kcrusselluniversity <kcrussell.university@gmail.com>
2023-12-26 11:52:01 +1100

    Write logic to store session data in backend database

--- src/pages/Workout/components/FinishSessionButton.jsx
+++ src/pages/Workout/components/FinishSessionButton.jsx
@@ -1,6 +1,8 @@
 import { useContext } from "react";
 import GreyButton from "../../../components/ui/GreyButton";
 import { exerciseModalContext } from "../../../context/exerciseModalContext";
+import { doc, setDoc } from "firebase/firestore";
+import { auth, db } from "../../../config/firebase";
 
 /**
  * FinishSessionButton component
@@ -12,6 +14,8 @@ import { exerciseModalContext } from "../../../context/exerciseModalContext";
 const FinishSessionButton = () => {
     // Destructure required context state
     const {
+        sessionExercises,
+        sessionStartTime,
         setExerciseModalState,
         removeWorkoutActive,
         removeSessionStartTime,
@@ -20,12 +24,38 @@ const FinishSessionButton = () => {
         setExerciseAdded,
     } = useContext(exerciseModalContext);
 
-    const handleClick = () => {
+    const handleClick = async () => {
         // Check if any exercises were added
+        const exercises = sessionExercises.getExercises();
+        const isExercisesAdded = Object.keys(exercises).length != 0;
+
         // If they have, update the state
-        // Upload data to firestore
-        // If not, update the relevant state accordingly
-        
+        if (isExercisesAdded) {
+            const startTimeObject = new Date(sessionStartTime);
+            const sessionUid = startTimeObject.getTime().toString();
+            const userUid = auth.currentUser.uid;
+            const userSessionObject = {
+                startTime: startTimeObject,
+                exercises: sessionExercises.getExercises(),
+            };
+
+            // Convert the custom objects into basic objects for storage
+            const basicObject = JSON.parse(JSON.stringify(userSessionObject));
+
+            // Upload data to firestore
+            try {
+                const userExerciseHistoryDocRef = doc(
+                    db,
+                    `users/${userUid}/exerciseHistory`,
+                    sessionUid
+                );
+
+                await setDoc(userExerciseHistoryDocRef, basicObject);
+            } catch (err) {
+                console.error(err);
+            }
+        }
+
         // Update state to end session and prepare for next session
         removeWorkoutActive();
         removeSessionStartTime();
