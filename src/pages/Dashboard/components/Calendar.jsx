e1033fafe21cb79271e6d28dae751a8fd2cd3a08
kcrusselluniversity <kcrussell.university@gmail.com>
2024-01-10 13:11:29 +1100

    Refactor db fetching logic into custom hook

--- src/pages/Dashboard/components/Calendar.jsx
+++ src/pages/Dashboard/components/Calendar.jsx
@@ -1,9 +1,6 @@
 import { DateCalendar, DayCalendarSkeleton } from "@mui/x-date-pickers";
-import { useEffect, useState } from "react";
-import { auth, db } from "../../../config/firebase";
-import { collection, query, getDocs } from "firebase/firestore";
 import CustomDayComponent from "./CustomDayComponent";
-import dayjs from "dayjs";
+import useFetchActiveDays from "../../../hooks/useFetchActiveDays";
 
 /**
  * Calendar Component
@@ -12,39 +9,7 @@ import dayjs from "dayjs";
  * calendar.
  */
 const Calendar = () => {
-    const [isLoading, setIsLoading] = useState(true);
-    const [activeDays, setActiveDays] = useState([]);
-
-    // Fetch the users history on mount, to be used to determine
-    // which days the user completed a gym session and hence whether to
-    // highlight that day on the calendar
-    useEffect(() => {
-        async function fetchActiveDays() {
-            const userUid = auth.currentUser.uid;
-            let activeDaysArray = [];
-
-            // Query db for this users exercise history
-            const q = query(collection(db, `users/${userUid}/exerciseHistory`));
-            const historySnapshot = await getDocs(q);
-
-            // Get the startTime from each session
-            historySnapshot.forEach((doc) => {
-                const { startTime } = doc.data();
-                activeDaysArray.push(startTime);
-            });
-
-            // Map the activeDay strings to a dayjs objects
-            activeDaysArray = activeDaysArray.map((activeDay) =>
-                dayjs(activeDay)
-            );
-
-            // Update the state
-            setActiveDays(activeDaysArray);
-            setIsLoading(false);
-        }
-
-        fetchActiveDays();
-    }, []);
+    const { isLoading, activeDays } = useFetchActiveDays();
 
     return (
         <DateCalendar
