b840b76b8e8826355e65ad3d1dd48f7d74394d15
kcrusselluniversity <kcrussell.university@gmail.com>
2024-01-10 19:11:57 +1100

    Add functionality to display user session history

--- src/pages/History/History.jsx
+++ src/pages/History/History.jsx
@@ -1,7 +1,28 @@
-const History = () => {
-    return <>
-    <h1>History</h1>
-    </>
-}
+import SessionHistorySummary from "../../components/ui/SessionHistorySummary";
+import usePaginateHistory from "./usePaginateHistory";
+import Loading from "../../components/ui/Loading";
+import "./historyPage.css";
+import { Button } from "@mui/material";
 
-export default History
+const History = () => {
+    const { currentHistoryResults, isLoading, isLastPage, handleNextClick } =
+        usePaginateHistory();
+
+    const historySummaryComponents = currentHistoryResults.map(
+        (session, index) => (
+            <SessionHistorySummary sessionObject={session} key={index} />
+        )
+    );
+
+    return (
+        <>
+            <h1>History</h1>
+            <div className="History__content">
+                {!isLastPage && <Button onClick={handleNextClick}>Next</Button>}
+                {isLoading ? <Loading /> : historySummaryComponents}
+            </div>
+        </>
+    );
+};
+
+export default History;
