a7f2de7e4a4787adccfe1649fd9c166b939dc93e
kcrusselluniversity <kcrussell.university@gmail.com>
2024-01-17 18:06:47 +1100

    Improve styling of component

--- src/components/ui/GifContainer.jsx
+++ src/components/ui/GifContainer.jsx
@@ -1,13 +1,15 @@
-import { Skeleton } from "@mui/material";
-import { GIF_SIZE_STANDARD } from "../../data/constants";
 import { bool, func, string } from "prop-types";
+import { Skeleton } from "@mui/material";
+import {
+    GIF_SIZE_STANDARD,
+    SKELETON_SCALING_FACTOR,
+} from "../../data/constants";
 
 /**
- * Gif Container
+ * Gif Container component
  *
- * This is a helper component for the ExerciseDetails componant.
- * It renders the exercise gif associated with the selected exercise,
- * and shows a skeleton loading image if the gif is still loading.
+ * This React component renders the exercise gif associated with the selected
+ * exercise, and shows a skeleton loading image if the gif is still loading.
  */
 const GifContainer = ({
     name,
@@ -27,14 +29,15 @@ const GifContainer = ({
                 alt={name}
                 src={exerciseGifUrl}
                 onLoad={() => setIsGifLoading(false)}
+                width={`${GIF_SIZE_STANDARD}px`}
             />
             {isGifLoading && (
                 <Skeleton
                     className="exerciseDetails__skeleton"
                     variant="rectangular"
                     data-testid="exerciseDetailsSkeleton"
-                    height={GIF_SIZE_STANDARD}
-                    width={GIF_SIZE_STANDARD}
+                    height={GIF_SIZE_STANDARD * SKELETON_SCALING_FACTOR}
+                    width={GIF_SIZE_STANDARD * SKELETON_SCALING_FACTOR}
                     sx={{
                         borderRadius: "1rem",
                         opacity: isGifLoading ? 1 : 0,
