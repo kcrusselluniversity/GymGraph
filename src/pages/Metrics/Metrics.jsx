fae6da00c662cd821d7c624e9d7147c5ea0d1abc
kcrusselluniversity <kcrussell.university@gmail.com>
2024-01-17 15:47:10 +1100

    Add component to metrics content component

--- src/pages/Metrics/Metrics.jsx
+++ src/pages/Metrics/Metrics.jsx
@@ -1,15 +1,26 @@
+import { useContext } from "react";
 import MetricsSearchBar from "./components/MetricsSearchBar";
 import MostRecentExercises from "./components/MostRecentExercises";
 import "./metrics.css";
+import { metricsContext } from "../../context/appContext";
+import SelectedExerciseMetrics from "./components/SelectedExerciseMetrics";
 
 const Metrics = () => {
+    const { selectedExercise } = useContext(metricsContext);
     return (
         <div className="metricsPage userPageGrid">
             <div className="metrics__header">
                 <MetricsSearchBar />
                 <MostRecentExercises />
             </div>
-            <div className="metrics__content"></div>
+
+            <div className="metrics__content">
+                {selectedExercise ? (
+                    <SelectedExerciseMetrics />
+                ) : (
+                    <h2>Select an exercise to see your metrics</h2>
+                )}
+            </div>
         </div>
     );
 };
