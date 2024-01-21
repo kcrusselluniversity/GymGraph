9f31db2c137495d83b83d3f734b67e179654915e
kcrusselluniversity <kcrussell.university@gmail.com>
2024-01-21 12:35:42 +1100

    Add graph component to this component

--- src/pages/Metrics/components/SelectedExerciseMetrics.jsx
+++ src/pages/Metrics/components/SelectedExerciseMetrics.jsx
@@ -1,6 +1,7 @@
 import { useContext } from "react";
 import { metricsContext } from "../../../context/appContext";
 import SelectedExerciseGif from "./SelectedExerciseGif";
+import ExerciseSetVolumePerSession from "./ExerciseSetVolumePerSession";
 
 const SelectedExerciseMetrics = () => {
     const { selectedExercise } = useContext(metricsContext);
@@ -8,6 +9,7 @@ const SelectedExerciseMetrics = () => {
     return (
         <div>
             <SelectedExerciseGif />
+            <ExerciseSetVolumePerSession selectedExercise={selectedExercise} />
         </div>
     );
 };
