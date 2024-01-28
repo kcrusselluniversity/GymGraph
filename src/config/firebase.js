47278e103cf08bf70a20a3900ce17ba0d2d83488
kcrusselluniversity <kcrussell.university@gmail.com>
2024-01-28 12:19:04 +1100

    Update firebase auth config

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -4,7 +4,7 @@ import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
     apiKey: "AIzaSyBm20LnuKowcEeV0R4UayexrsUvQaAmZr4",
-    authDomain: "gymgraph-ac95d.firebaseapp.com",
+    authDomain: "https://gymgraph.netlify.app/",
     projectId: "gymgraph-ac95d",
     storageBucket: "gymgraph-ac95d.appspot.com",
     messagingSenderId: "569702322733",
